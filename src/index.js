2a209dab69a081bcb7fbdc8a705a3948dc846b2d
Victor Hugo <victorq32@gmail.com>
2023-03-06 23:44:24 -0300

    Aulua 12 - Scalar Types no GraphQl

--- src/index.js
+++ src/index.js
@@ -3,11 +3,26 @@ import { ApolloServer, gql } from 'apollo-server';
 const server = new ApolloServer({
   typeDefs: gql`
     type Query {
-      hello: String
+      id: ID!
+      name: String!
+      age: Int!
+      average: Float!
+      married: Boolean!
+      arrayString: [String!]!
     }
   `,
+  resolvers: {
+    Query: {
+      id: () => 'dsa54d65asd46asdd',
+      name: () => 'Victor Hugo',
+      age: () => 31,
+      average: () => 50.55,
+      married: () => false,
+      arrayString: () => ['A', 'B'],
+    },
+  },
 });
 
-server.listen(4003).then(({ url }) => {
+server.listen(4004).then(({ url }) => {
   console.log(`Server listening on url ${url}`);
 });
